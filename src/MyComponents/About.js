1009fd754e423e400e2dfa02a2d62a6b16ae6419
Mudassir55 <mudassirhassan558@gmail.com>
2022-12-23 19:09:52 +0500

    second commit

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
+//this is about page
 
 export const About = () => {
     return (
         <div className='container'>
+
             <h3>About us</h3>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore vitae accusantium magnam doloribus fugiat culpa molestiae dolores ratione quo suscipit, id recusandae dolorum iste eos doloremque eaque praesentium, ipsam a.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore vitae accusantium magnam doloribus fugiat culpa molestiae dolores ratione quo suscipit, id recusandae dolorum iste eos doloremque eaque praesentium.</p>
 
         </div>
     )
